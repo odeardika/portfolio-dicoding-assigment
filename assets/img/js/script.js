618dfa875c9a7c86cb816c3ed20994a70ee82cdb
Ode Ardika <dirfanardika@gmail.com>
2024-02-14 01:58:49 +0800

    Done doing change image in project section make thw next arrow fix the error when clicking on different element in the arrow svg

--- assets/img/js/script.js
+++ assets/img/js/script.js
@@ -1,13 +1,43 @@
-const backArrowCashirDesktop = document.getElementById('back-cashir-desktop')
+const backArrow = document.querySelectorAll('.back')
+backArrow.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        //get div element based of element that has been clicked
+        let project 
+        (event.target.nodeName == 'svg')? project = event.target.parentElement.parentElement.parentElement : project = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+        const projectClass = project.getAttribute('class')
+        let totalImage
+        (projectClass === 'cashir-desktop')? totalImage = 4: totalImage = 2
 
-backArrowCashirDesktop.addEventListener('click', ()=>{
-    const img = document.getElementById('cashir-desktop-img')
-    let srcAtribut = img.getAttribute('src')
-    
-    if(srcAtribut.charAt(srcAtribut.length-5) == 1){
-        img.setAttribute('src',`assets\\img\\project\\cashir-desktop\\${4}.png`)
-    }else{
-        img.setAttribute('src',`assets\\img\\project\\cashir-desktop\\${(srcAtribut.charAt(srcAtribut.length-5))-1}.png`)
-    }
-    
+        const img = project.querySelector('div').querySelector('img')
+        const curretImgPath = img.getAttribute('src')
+        const curretImgIndex = curretImgPath.charAt(curretImgPath.length-5)
+        
+        if(curretImgIndex == 1){
+            img.setAttribute('src',`assets\\img\\project\\${projectClass}\\${totalImage}.png`)
+        }else{
+            img.setAttribute('src',`assets\\img\\project\\${projectClass}\\${curretImgIndex-1}.png`)
+        }
+    })
+})
+
+const nextArrow = document.querySelectorAll('.next')
+nextArrow.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        //get div element based of element that has been clicked
+        let project 
+        (event.target.nodeName == 'svg')? project = event.target.parentElement.parentElement.parentElement : project = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+        const projectClass = project.getAttribute('class')
+        let totalImage
+        (projectClass === 'cashir-desktop')? totalImage = 4: totalImage = 2
+
+        const img = project.querySelector('div').querySelector('img')
+        const curretImgPath = img.getAttribute('src')
+        const curretImgIndex = curretImgPath.charAt(curretImgPath.length-5)
+
+        if(curretImgIndex == totalImage){
+            img.setAttribute('src',`assets\\img\\project\\${projectClass}\\${1}.png`)
+        }else{
+            img.setAttribute('src',`assets\\img\\project\\${projectClass}\\${parseInt(curretImgIndex)+1}.png`)
+        }
+    })
 })
